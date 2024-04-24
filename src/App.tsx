3728d826f65cb5e1e3d95d9cc8f660982bd4e51c
Gudynin Danila <dgudynin@ai-space.ru>
2024-04-24 19:20:05 +0300

    add events and input for key thread

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
-import './App.css';
-import {Map, YMaps} from "react-yandex-maps";
-import {useState} from "react";
-import {Maximize} from "lucide-react";
-
+import { Maximize } from 'lucide-react'
+import { useState } from 'react'
+import { Map, YMaps } from 'react-yandex-maps'
+import './App.css'
+import Events from './components/events/events'
 function App() {
-  const [size, setSize] = useState<string>("medium")
-  
-  return (
-    <div className="App">
-      <div className={`${size == 'medium' ? 'camera-container-medium' : 'camera-container-large'}`}>
-        <div className='camera-blocks'>
-          <div className='camera'>КАМЕРА</div>
-          <span className='changeSize'
-                onClick={() => setSize(prev => prev === 'medium' ? ' large' : 'medium')}>
-            <Maximize/>
-          </span>
-        </div>
-      </div>
-      <YMaps>
-        <Map defaultState={{center: [55.75, 37.57], zoom: 9}} className='map'/>
-      </YMaps>
-    </div>
-  );
+	const [size, setSize] = useState<string>('medium')
+
+	return (
+		<div>
+			<div>
+				<div
+					className={`${
+						size == 'medium'
+							? 'camera-container-medium'
+							: 'camera-container-large'
+					}`}
+				>
+					<div className='camera-blocks'>
+						<div className='camera'>КАМЕРА</div>
+						<span
+							className='resize'
+							onClick={() =>
+								setSize(prev => (prev === 'medium' ? ' large' : 'medium'))
+							}
+						>
+							<Maximize className='resize-icon' />
+						</span>
+					</div>
+					<div className='input-area'>
+						<div className='input-block'>
+							<label htmlFor='thread'>Введите ключ потока:</label>
+							<input
+								type='text'
+								name='keyInput'
+								id='thread'
+								className='input'
+								placeholder='Ваш ключ...'
+							/>
+						</div>
+					</div>
+				</div>
+				<Events />
+			</div>
+			<YMaps>
+				<Map
+					defaultState={{ center: [55.75, 37.57], zoom: 9 }}
+					className='map'
+				/>
+			</YMaps>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/events/events.data.ts
@@ -0,0 +1,45 @@
+export interface Event {
+	location: string
+	time: string
+	eventContent: string
+	eventType: string
+}
+
+export const eventsList: Event[] = [
+	{
+		location: 'МАИ, Кпп 1',
+		time: '12.15',
+		eventContent: 'Пожар',
+		eventType: 'Огонь',
+	},
+	{
+		location: 'МАИ, Кпп 2',
+		time: '12.15',
+		eventContent: 'Драка',
+		eventType: 'Драка',
+	},
+	{
+		location: 'МАИ, Кпп 3',
+		time: '12.15',
+		eventContent: 'Дым',
+		eventType: 'Огонь',
+	},
+	{
+		location: 'МАИ, Кпп 5',
+		time: '12.15',
+		eventContent: 'Алкоголь',
+		eventType: 'Нарушение',
+	},
+	{
+		location: 'МАИ, Кпп 6',
+		time: '12.15',
+		eventContent: 'Сигареты',
+		eventType: 'Нарушение',
+	},
+	{
+		location: 'МАИ, Кпп 7',
+		time: '12.15',
+		eventContent: 'Машина проехала кпп без досмотра',
+		eventType: 'Проникновение',
+	},
+]
